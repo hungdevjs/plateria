086d9eb19b84a1202bd41b12425a935a32bd6e0d
hungdevjs <hungdev.js@gmail.com>
2021-12-27 09:24:50 +0700

    button component

---
+++ app/components/Button.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+import { Colors, FontSizes } from "../utils/constants";
+
+const Button = ({ outline, backgroundColor, color, text, onPress }) => {
+  const renderContainerStyle = () => {
+    if (outline)
+      return {
+        backgroundColor: "transparent",
+        borderWidth: 2,
+        borderColor: backgroundColor,
+      };
+    if (backgroundColor) return { backgroundColor };
+    return {};
+  };
+
+  return (
+    <TouchableOpacity
+      style={[styles.container, renderContainerStyle()]}
+      onPress={onPress}
+    >
+      <Text style={[styles.text, color && { color }]}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 60,
+    borderRadius: 4,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors.Primary,
+  },
+  text: {
+    fontWeight: "700",
+    color: Colors.White,
+    fontSize: FontSizes.Big,
+  },
+});
--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,3 +1,12 @@
+import { Dimensions } from "react-native";
+import Constants from "expo-constants";
+
+const { width, height } = Dimensions.get("window");
+
+export const screenWidth = width < height ? width : height;
+export const screenHeight = width < height ? height : width;
+export const appHeight = height - Constants.statusBarHeight;
+
 export const BaseUrl = "http://localhost:8888/api/v1";
 
 export const AccessToken = "AccessToken";
@@ -16,4 +25,13 @@ export const Screens = {
 export const Colors = {
   Overlay: "rgba(0, 0, 0, 0.4)",
   Red: "#ff7979",
+  White: "#fff",
+  Primary: "#3b5998",
+};
+
+export const FontSizes = {
+  Small: 12,
+  Normal: 14,
+  Big: 16,
+  Huge: 18,
 };
