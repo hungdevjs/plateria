898081c9a1c31ddef9d9566b690b7058ab91ae6f
hungdevjs <hungdev.js@gmail.com>
2021-12-30 11:19:58 +0700

    add images && update seed data

--- server/seed.js
+++ server/seed.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import _ from "lodash";
 dotenv.config();
 
 import Plant from "./models/plant.model.js";
@@ -16,13 +17,16 @@ connection.once("open", () => {
   init();
 });
 
+const indexes = [1, 2, 3, 4, 5, 6, 7, 8];
+
 const init = async () => {
   console.log("Init db...");
+  console.log("Init plants...");
   const defaultPlant = await Plant.findOne({ isDefault: true }).lean();
   if (!defaultPlant) {
     const plant = new Plant({
-      name: "Plant 1",
-      image: "",
+      name: "Plant",
+      image: "Plant.png",
       type: PlantTypes.Indoor,
       level: PlantLevels.Easy,
       price: 100,
@@ -31,13 +35,34 @@ const init = async () => {
 
     await plant.save();
   }
-  console.log("Init default plant done!");
 
+  for (const index of indexes) {
+    const plant = await Plant.findOne({ image: `Plant${index}.png` }).lean();
+    if (!plant) {
+      const newPlant = new Plant({
+        name: `Plant ${index}`,
+        image: `Plant${index}.png`,
+        type: _.sample([PlantTypes.Indoor, Plant.Outdoor]),
+        level: _.sample([
+          PlantLevels.Easy,
+          PlantLevels.Medium,
+          PlantLevels.Hard,
+        ]),
+        price: 100 * (index + 1),
+      });
+
+      await newPlant.save();
+    }
+  }
+
+  console.log("Init plants done!");
+
+  console.log("Init pots...");
   const defaultPot = await Pot.findOne({ isDefault: true }).lean();
   if (!defaultPot) {
     const pot = new Pot({
-      name: "Pot 1",
-      image: "",
+      name: "Pot",
+      image: "Pot.png",
       price: 10,
       isDefault: true,
     });
@@ -45,14 +70,28 @@ const init = async () => {
     await pot.save();
   }
 
-  console.log("Init default pot done!");
+  for (const index of indexes) {
+    const pot = await Pot.findOne({ image: `Pot${index}.png` }).lean();
+    if (!pot) {
+      const newPot = new Pot({
+        name: `Pot ${index}`,
+        image: `Pot${index}.png`,
+        price: 10 * (index + 1),
+      });
 
+      await newPot.save();
+    }
+  }
+
+  console.log("Init pots done!");
+
+  console.log("Init backgrounds...");
   const defaultBackground = await Background.findOne({
     isDefault: true,
   }).lean();
   if (!defaultBackground) {
     const background = new Background({
-      image: "",
+      image: "Background.png",
       price: 100,
       isDefault: true,
     });
@@ -60,5 +99,19 @@ const init = async () => {
     await background.save();
   }
 
-  console.log("Init default background done!");
+  for (const index of indexes) {
+    const background = await Background.findOne({
+      image: `Background${index}.png`,
+    }).lean();
+    if (!background) {
+      const newBackground = new Background({
+        image: `Background${index}.png`,
+        price: 100 * (index + 1),
+      });
+
+      await newBackground.save();
+    }
+  }
+
+  console.log("Init backgrounds done!");
 };
